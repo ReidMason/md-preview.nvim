f7e275ff529acde828ed0be1d4bbd53f4a788981
Reid Mason <maddogshain132@gmail.com>
2023-12-22 19:43:32 +0000

    Added user command to close preview

--- lua/md-preview.lua
+++ lua/md-preview.lua
@@ -7,6 +7,12 @@ local showPreview = function()
 	local currWin = vim.api.nvim_get_current_win()
 	local curr_buff = vim.api.nvim_get_current_buf()
 	local curr_file = vim.api.nvim_buf_get_name(curr_buff)
+
+	-- Don't run for non markdown files
+	if string.match(curr_file, "^.*.md$") == nil then
+		return
+	end
+
 	local x = vim.api.nvim_buf_get_lines(curr_buff, 0, -1, false)
 
 	if M._win == nil then
@@ -39,7 +45,13 @@ M.setup = function()
 			buffer = curr_buff,
 			callback = showPreview,
 		})
+
 		showPreview()
+	end, {})
+
+	vim.api.nvim_create_user_command("CloseMdPreview", function()
+		local buff = vim.api.nvim_win_get_buf(M._win)
+		vim.api.nvim_buf_delete(buff, {})
 	end, {})
 end
 
